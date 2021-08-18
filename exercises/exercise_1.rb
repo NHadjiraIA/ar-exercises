1bdce28a5ad92d130eccd278466c97bb7661042b
NHadjiraIA <nadjibbenkhellat@Nadjibs-MacBook-Pro.local>
2021-08-18 19:42:30 -0400

    exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,27 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnaby = Store.create(
+  name: 'burnaby',
+  annual_revenue: '300000',
+  mens_apparel: TRUE,
+  womens_apparel: TRUE
+)
+
+richmond = Store.create(
+  name: 'richmond',
+  annual_revenue: '1260000',
+  mens_apparel: TRUE,
+  womens_apparel: TRUE
+)
+
+gastown = Store.create(
+  name: 'gastown',
+  annual_revenue: '190000',
+  mens_apparel: TRUE,
+  womens_apparel: TRUE
+)
+ 
+num_of_stores = Store.count
+
+puts "The number of stores are : #{num_of_stores}"
